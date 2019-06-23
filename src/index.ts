e64558d9a20d71bd6be857023b54ad09161f9c29
Steph <N/A>
2019-06-23 19:17:06 -0400

    New methods set() and fetch() in User to coordinate syncing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { User } from './models/User'
 
-const user = new User({ name: 'NoID', age: 99 })
-console.log(user.get('name'))
-user.on('change', () => console.log('changed!'))
-user.trigger('change')
+const user = new User({ id: 1 })
+
+user.on('change', () => {
+  console.log(user)
+})
+
+user.fetch()
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 import { Eventing } from './Eventing'
 import { Sync } from './Sync'
 import { Attributes } from './Attributes'
+import { AxiosResponse } from 'axios';
 
 export interface UserProps {
   id?: number
@@ -8,7 +9,7 @@ export interface UserProps {
   age?: number
 }
 
-const rootUrl = 'http://localhost:3000/user'
+const rootUrl = 'http://localhost:3000/users'
 
 export class User {
   public events: Eventing = new Eventing()
@@ -29,5 +30,24 @@ export class User {
 
   get get() {
     return this.attributes.get
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update)
+    this.events.trigger('change')
+  }
+
+  fetch(): void {
+    const id = this.get('id')
+
+    if (typeof id !== 'number') {
+      throw new Error('Can not fetch without an id')
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      // Use class set method instead of attributes set method
+      // in order to trigger change event
+      this.set(response.data)
+    })
   }
 }
