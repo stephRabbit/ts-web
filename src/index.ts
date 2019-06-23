a2dd484002bf78f9f5c1d60f436673a4bbe1d932
Steph <N/A>
2019-06-22 23:09:10 -0400

    Accessor test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { User } from './models/User'
 
 const user = new User({ name: 'NoID', age: 99 })
-user.events.on('change', () => {
-  console.log('CHANGED!')
-})
 
-user.events.trigger('change')
+// Accessors
+class Person {
+  constructor(public firstName: string, public lastName: string) { }
+
+  // getter for retrieving infromation - accessor
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+const person = new Person('Gizmo', 'Jones')
+// Gets invoked and returns string
+console.log(person.fullName)
