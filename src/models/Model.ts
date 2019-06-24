92021bb53dd65db5a65b33c768fff7f17ee8672c
Steph <N/A>
2019-06-23 21:01:45 -0400

    Extracting User in to Model class and using inheritance

---
+++ src/models/Model.ts
@@ -0,0 +1,94 @@
+import {
+  AxiosPromise,
+  AxiosResponse,
+  AxiosError
+} from 'axios'
+interface ModelAttributes<T> {
+  get<K extends keyof T>(key: K): T[K]
+  getAll(): T
+  set(update: T): void
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise
+  save(data: T): AxiosPromise
+}
+
+interface Events {
+  on(eventName: string, callback: Callback): void
+  trigger(eventName: string): void
+}
+
+interface HasId {
+  id?: number
+}
+
+type Callback = () => void
+
+/**
+ * @params <T> -shape of data constraint HasId
+ */
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) { }
+
+  // Getters
+  get on() {
+    return this.events.on
+  }
+
+  get trigger() {
+    return this.events.trigger
+  }
+
+  get get() {
+    return this.attributes.get
+  }
+
+  /**
+   * Call attribute set method with update
+   * and call events trigger method with 'change'
+   * @param update - shape of UserProps
+   */
+  set(update: T): void {
+    this.attributes.set(update)
+    this.events.trigger('change')
+  }
+
+  /**
+   * Coordinate sync fetch with id and call
+   * set in current class
+   */
+  fetch(): void {
+    const id = this.get('id')
+
+    if (typeof id !== 'number') {
+      throw new Error('Can not fetch without an id')
+    }
+
+    this.sync.fetch(id)
+      .then((response: AxiosResponse): void => {
+        // Use class set method instead of attributes set method
+        // in order to trigger change event
+        this.set(response.data)
+      })
+  }
+
+  /**
+   * Save all user properties, on sucess
+   * trigger save on error trigger error
+   */
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save')
+      })
+      .catch((err: AxiosError) => {
+        console.error(err)
+        this.trigger('error')
+      })
+  }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
-import { Eventing } from './Eventing'
-import { Sync } from './Sync'
+import { Model } from './Model'
+import { ApiSync } from './ApiSync'
 import { Attributes } from './Attributes'
-import { AxiosResponse, AxiosError } from 'axios';
+import { Eventing } from './Eventing'
 
 export interface UserProps {
   id?: number
@@ -11,69 +11,12 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users'
 
-export class User {
-  public events: Eventing = new Eventing()
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
-  public attributes: Attributes<UserProps>
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs)
-  }
-
-  // Getters
-  get on() {
-    return this.events.on
-  }
-
-  get trigger() {
-    return this.events.trigger
-  }
-
-  get get() {
-    return this.attributes.get
-  }
-
-  /**
-   * Call attribute set method with update
-   * and call events trigger method with 'change'
-   * @param update - shape of UserProps
-   */
-  set(update: UserProps): void {
-    this.attributes.set(update)
-    this.events.trigger('change')
-  }
-
-  /**
-   * Coordinate sync fetch with id and call
-   * set in current class
-   */
-  fetch(): void {
-    const id = this.get('id')
-
-    if (typeof id !== 'number') {
-      throw new Error('Can not fetch without an id')
-    }
-
-    this.sync.fetch(id)
-      .then((response: AxiosResponse): void => {
-        // Use class set method instead of attributes set method
-        // in order to trigger change event
-        this.set(response.data)
-      })
-  }
-
-  /**
-   * Save all user properties, on sucess
-   * trigger save on error trigger error
-   */
-  save(): void {
-    this.sync.save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger('save')
-      })
-      .catch((err: AxiosError) => {
-        console.error(err)
-        this.trigger('error')
-      })
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    )
   }
 }
