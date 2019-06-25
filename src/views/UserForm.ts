596e7b415e71c75da6464a57f0e4570798c55257
Steph <N/A>
2019-06-24 21:09:43 -0400

    Event binding helper added to for render method

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,20 +1,47 @@
 export class UserForm {
   constructor(public parent: HTMLElement) { }
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick
+    }
+  }
+
+  onButtonClick(): void {
+    console.log('Clicked!')
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input type="text">
+        <button type="button">Click</button>
       </div>
     `
+  }
+
+  /**
+   * Iterate over eventsMap split the key ':'
+   * Iterate over fragment and add event and callback
+   * @param frag - document fragment
+   */
+  bindEvents(frag: DocumentFragment): void {
+    const eventsMap = this.eventsMap()
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':')
+      Array.from(frag.querySelectorAll(selector)).forEach((el: HTMLElement): void => {
+        el.addEventListener(eventName, eventsMap[eventKey])
+      })
+    }
   }
 
   render(): void {
     if ('content' in document.createElement('template')) {
       const tempEl = document.createElement('template')
       tempEl.innerHTML = this.template()
-
+      this.bindEvents(tempEl.content)
       this.parent.append(tempEl.content)
     } else {
       console.log('Sorry, your browser does not support this feature!')
